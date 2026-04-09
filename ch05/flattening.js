da1c1007730cb11baaacd18f1c81e3c04a2db924
neo-dev <sei133dirsoa@gmail.com>
2026-04-08 23:32:06 -0300

    add ch05, with flattening exercise where the reduce method of arrays is studied for a better abstraction over looping.

---
+++ ch05/flattening.js
@@ -0,0 +1,25 @@
+/*
+        Flattening
+        Use the reduce method in combination with the concat method to “flatten” an array of arrays into a single array that has all the elements of the original arrays.
+*/
+
+let arrayOfArrays = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+
+let array = arrayOfArrays.reduce((accum, current) => { return accum.concat(current) }, [])
+
+console.table(array);
+
+
+let arrayNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+let sum = arrayNums.reduce((sum, current) => { return sum + current }, 0);
+
+console.log(sum);
+
+let arrayProperties = [["name", "snake"], ["afiliation", "msf"]];
+
+let snake = arrayProperties.reduce((obj, current) => { obj[current[0]] = current[1]; return obj; }, {});
+
+console.log(snake);
+
