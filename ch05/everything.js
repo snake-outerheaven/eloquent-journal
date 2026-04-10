da84230039a7a9c08fb70cff9a07cc4db4a7c2e2
neo-dev <sei133dirsoa@gmail.com>
2026-04-09 22:38:19 -0300

    Add everything exercise

---
+++ ch05/everything.js
@@ -0,0 +1,23 @@
+/*
+Implement `every` as a function that takes an array and a predicate function as parameters.
+Write two versions:
+1. One using a loop
+2. One using the `some` method
+*/
+
+
+function every(arr, func) {
+        for (let element of arr)
+                if (!func(element))
+                        return false;
+        return true;
+}
+
+function everySome(arr, func) {
+        return !arr.some(element => !func(element))
+}
+
+console.log(every([1, 2, 3, 4, 5, 6], n => n * null));
+console.log(everySome([1, 2, 3, 4, 5, 6], n => n * 2));
+
+console.log([1, 2, 3, 4, 5, 6, 7].every(x => x + 1))
