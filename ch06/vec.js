917a9fd4b5658f4f06b60f835713a011fb911326
neo-dev <sei133dirsoa@gmail.com>
2026-04-27 12:10:24 -0300

    first use of Emacs on this repo

--- ch06/vec.js
+++ ch06/vec.js
@@ -1,71 +1,71 @@
 /*
-Write a class Vec that represents a vector in two-dimensional space. 
-It takes x and y parameters (numbers), that it saves to properties of the same name.
+  Write a class Vec that represents a vector in two-dimensional space. 
+  It takes x and y parameters (numbers), that it saves to properties of the same name.
 
-Give the Vec prototype two methods, plus and minus, that take another vector 
-as a parameter and return a new vector that has the sum or difference of the two vectors’ 
-(this and the parameter) x and y values.
+  Give the Vec prototype two methods, plus and minus, that take another vector 
+  as a parameter and return a new vector that has the sum or difference of the two vectors’ 
+  (this and the parameter) x and y values.
 
-Add a getter property length to the prototype that computes the length of the vector—
-that is, the distance of the point (x, y) from the origin (0, 0).
+  Add a getter property length to the prototype that computes the length of the vector—
+  that is, the distance of the point (x, y) from the origin (0, 0).
 */
 
 
 class Vec {
-        #x;
-        #y;
+    #x;
+    #y;
 
-        constructor(x, y) {
-                if (typeof x !== "number" || typeof y !== "number") {
-                        throw new TypeError("x and y must be numbers");
-                }
-                this.#x = x;
-                this.#y = y;
+    constructor(x, y) {
+        if (typeof x !== "number" || typeof y !== "number") {
+            throw new TypeError("x and y must be numbers");
         }
+        this.#x = x;
+        this.#y = y;
+    }
 
-        get x() {
-                return this.#x;
-        }
+    get x() {
+        return this.#x;
+    }
 
-        set x(value) {
-                if (typeof value !== "number") {
-                        throw new TypeError("x must be a number");
-                }
-                this.#x = value;
+    set x(value) {
+        if (typeof value !== "number") {
+            throw new TypeError("x must be a number");
         }
+        this.#x = value;
+    }
 
-        get y() {
-                return this.#y;
-        }
+    get y() {
+        return this.#y;
+    }
 
-        set y(value) {
-                if (typeof value !== "number") {
-                        throw new TypeError("y must be a number");
-                }
-                this.#y = value;
+    set y(value) {
+        if (typeof value !== "number") {
+            throw new TypeError("y must be a number");
         }
+        this.#y = value;
+    }
 
-        static isVec(obj) {
-                return obj instanceof Vec;
-        }
+    static isVec(obj) {
+        return obj instanceof Vec;
+    }
 
-        plus(vec) {
-                if (!Vec.isVec(vec)) {
-                        throw new TypeError("Argument must be a Vec instance");
-                }
-                return new Vec(this.#x + vec.#x, this.#y + vec.#y);
+    plus(vec) {
+        if (!Vec.isVec(vec)) {
+            throw new TypeError("Argument must be a Vec instance");
         }
+        return new Vec(this.#x + vec.#x, this.#y + vec.#y);
+    }
 
-        minus(vec) {
-                if (!Vec.isVec(vec)) {
-                        throw new TypeError("Argument must be a Vec instance");
-                }
-                return new Vec(this.#x - vec.#x, this.#y - vec.#y);
+    minus(vec) {
+        if (!Vec.isVec(vec)) {
+            throw new TypeError("Argument must be a Vec instance");
         }
+        return new Vec(this.#x - vec.#x, this.#y - vec.#y);
+    }
 
-        get length() {
-                return Math.sqrt(this.#x * this.#x + this.#y * this.#y);
-        }
+    get length() {
+        return Math.sqrt(this.#x * this.#x + this.#y * this.#y);
+    }
 }
 
 let vec1 = new Vec(12, 34);
